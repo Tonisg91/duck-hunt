ecfb868a8b9a0cb75c8d18377752271f84534085
Toni <sancheztoni.garcia@gmail.com>
2020-04-30 10:37:14 +0200

    canvas background created

--- src/JS/gameBoard.js
+++ src/JS/gameBoard.js
@@ -1,24 +1,21 @@
-/* class Board {
-    constructor() {
-        this.canvas = document.getElementById('game-board');
-        this.ctx = this.canvas.getContext('2d');
-        this.width = this.canvas.width; //1200px
-        this.height = this.canvas.height; // 700px
-    }
-
-    green() {
-        this.ctx.fillStyle = 'lightgreen';
-        this.ctx.fillRect(0, this.height, this.width, -100);
-    }
-    sky() {
-        this.ctx.fillStyle = 'lightblue';
-        this.ctx.fillRect(0, this.height - 100, this.width, -600)
-    }
-    background() {
-        this.green();
-        this.sky();
-
-    }
+class Board {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.width = this.canvas.width; //1200px
+    this.height = this.canvas.height; // 700px
+  }
+  buildBackground() {
+    //Green zone occupies the 15% of canvas
+    this.ctx.fillStyle = "lightgreen";
+    this.ctx.fillRect(0, this.height, this.width, -this.height * 0.15);
+    //Blue zone (Sky) the rest 85%
+    this.ctx.fillStyle = "lightblue";
+    this.ctx.fillRect(
+      0, //X start
+      this.height - this.height * 0.15, //Y start
+      this.width, //X finish
+      -this.height * 0.85 //Y finish
+    );
+  }
 }
- */
-
--- src/JS/main.js
+++ src/JS/main.js
@@ -1,46 +1,46 @@
 const main = () => {
-    const buildDom = (html) => {
-        const main = document.querySelector("main");
-        main.innerHTML = html;
-    };
+  const buildDom = (html) => {
+    const main = document.querySelector("main");
+    main.innerHTML = html;
+  };
 
-
-    function buildStartScreen() {
-        console.log('testttt');
-        buildDom(
-            `
+  function buildStartScreen() {
+    buildDom(
+      `
                 <section class="startScreen">
                     <h1>DUCK<br><span>HUNT</span></h1>
                     <button id="one-player">1 Player</button>
                     <button id="two-players">2 Players</button>
                 </section>
-            `);//buildDom
 
-        const startButtons = document.querySelectorAll('button');
-        startButtons[0].addEventListener('click', buildGameBoard);
-        startButtons[1].addEventListener('click', buildGameBoard);
-    };// buildStartScreen
+            `
+    ); //buildDom
 
+    const startButtons = document.querySelectorAll("button");
+    startButtons[0].addEventListener("click", buildGameBoard);
+    startButtons[1].addEventListener("click", buildGameBoard);
+  } // buildStartScreen
 
-    const buildGameBoard = () => {
-        buildDom(`
+  const buildGameBoard = () => {
+    buildDom(`
              <section class="game-board">
                  <canvas></canvas>
              </section>
-         `);//buildDom
+         `); //buildDom
 
-        const width = document.querySelector(".game-board").offsetWidth;
-        const height = document.querySelector(".game-board").offsetHeight;
+    const width = document.querySelector(".game-board").offsetWidth;
+    const height = document.querySelector(".game-board").offsetHeight;
 
-        const canvasElement = document.querySelector("canvas");
+    const canvasElement = document.querySelector("canvas");
 
-        canvasElement.setAttribute("width", width);
-        canvasElement.setAttribute("height", height);
-    }
+    canvasElement.setAttribute("width", width);
+    canvasElement.setAttribute("height", height);
 
-    buildStartScreen();
+    const backGround = new Board(canvasElement);
+    backGround.buildBackground();
+  };
 
+  buildStartScreen();
+}; // main closing
 
-}// main closing
-
-window.addEventListener('load', main);
+window.addEventListener("load", main);
