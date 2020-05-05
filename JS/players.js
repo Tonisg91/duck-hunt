084238f3527c8c488da2feb01c2ebd6a1190feaa
Toni <sancheztoni.garcia@gmail.com>
2020-05-05 22:23:03 +0200

    def

--- JS/players.js
+++ JS/players.js
@@ -15,7 +15,7 @@ class Player {
     const sizeX = this.sizeX;
     const sizeY = this.sizeY;
     const playerImg = new Image();
-    playerImg.src = "../views/images/player.gif";
+    playerImg.src = "../views/images/player.png";
     playerImg.onload = function () {
       ctx.drawImage(playerImg, xPosition, yPosition, sizeX, sizeY);
     };
